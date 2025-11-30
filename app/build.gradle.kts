e7ca0293b89139f6511ad5c64ed6f777661f25c5
Olegueyan <gsourtg2cer@gmail.com>
2025-11-30 20:39:24 +0100

    I pray that it works 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     implementation("org.mariadb.jdbc:mariadb-java-client:3.1.4")
-    implementation("com.mysql:mysql-connector-j:8.4.0")
 
     // SLF4J API
     implementation("org.slf4j:slf4j-api:2.0.9")
